7ce881212582bf788ead49c03a00834e1b9f140b
jjswift <supercheater2018@yandex.ru>
2022-03-02 05:46:12 +0300

    Start structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+
+export const App: FC = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
