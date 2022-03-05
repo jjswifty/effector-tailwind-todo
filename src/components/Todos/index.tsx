a64de0bf3dc36e8b9932a9ef95a3e8accbb50608
jjswift <supercheater2018@yandex.ru>
2022-03-05 19:32:30 +0300

    Add todo component with id check

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,10 +1,48 @@
-import {FC} from "react";
+import {ChangeEvent, FC, useEffect} from "react";
+import {useStore} from "effector-react";
+import {$isIdExist, $todos, $userId, componentMounted, idChanged, idConfirmed} from "@/components/Todos/todos";
 
-export const Todos: FC = ({
+export const Todos: FC = ({}) => {
 
-}) => {
+    const todos = useStore($todos)
+    const isIdExist = useStore($isIdExist)
+    const userId = useStore($userId)
 
-    return <div>
+    useEffect(() => {
+        componentMounted(true)
+    }, [])
+
+    const idChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        idChanged(Number(e.target.value))
+    }
+
+    const idConfirmedHandler = () => {
+        idConfirmed()
+    }
+
+    return <div
+        className={`min-w-[700px] min-h-${isIdExist ? '[500px]' : 'fit'} flex flex-col items-center justify-between`}
+    >
+        {
+            isIdExist ? <>
+                <h1 className='text-3xl'>Your todos</h1>
+                {
+                    todos.length > 0
+                        ? todos.map(e => <div>
+                            <p>{e.title}</p>
+                        </div>)
+                        : <h1 className='text-4xl'>
+                            Nothing here. Get them now!
+                        </h1>
+                }
+                <input/>
+            </>
+            : <>
+                <p>Give yourself a numeric ID</p>
+                <input type="number" onChange={idChangeHandler} value={userId?.toString()}/>
+                <button onClick={idConfirmedHandler}>OK</button>
+            </>
+        }
 
     </div>
 }
