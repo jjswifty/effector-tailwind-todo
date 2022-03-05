4e4f954b190fbe88f69b23a1f7d268116b9eb0a7
jjswift <supercheater2018@yandex.ru>
2022-03-05 19:31:34 +0300

    TodoPage inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import {FC} from "react";
 import {MainLayout} from "@/components/Layouts/MainLayout";
+import {TodoPage} from "@/pages/TodoPage";
 
 
 
 export const App: FC = () => {
     return (
         <MainLayout backgroundColor='#'>
-            <h1 className="text-3xl">
-                fghjjcfghfghfghfghfghfghfghf
-            </h1>
+            <TodoPage />
         </MainLayout>
     )
 }
